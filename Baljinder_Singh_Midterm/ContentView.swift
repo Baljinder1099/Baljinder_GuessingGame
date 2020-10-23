225c52b36100647db9695f3a6e6ee86a317249c0
Baljinder <sing8665@sheridancollege.ca>
2020-10-22 21:21:40 -0400

    Initial Commit

---
+++ Baljinder_Singh_Midterm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Baljinder_Singh_Midterm
+//
+//  Created by Baljinder Singh on 2020-10-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
